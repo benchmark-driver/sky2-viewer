6792c32b95c03f92dad8ece3d6997166693242d5
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-16 01:02:49 -0700

    Maybe commiting here is too much

--- bin/build.sh
+++ bin/build.sh
@@ -18,19 +18,3 @@ env \
   RESULT_PATH="$result_path" \
   TZ="Asia/Tokyo" \
   bundle exec middleman build
-
-if [[ "$WERCKER_GIT_BRANCH" = "master" ]]; then
-  result_revision="$(git -C "$result_path" rev-parse HEAD)"
-
-  # Commit changes
-  cd "$pages_path"
-  git add .
-  if ! git diff-index --quiet HEAD --; then
-    git config --global user.email "sky2-viewer@benchmark-driver.github.io"
-    git config --global user.name "sky2-viewer"
-    git commit -m "Automatic deployment by Wercker
-
-See: https://github.com/benchmark-driver/sky2-result/commit/${result_revision}"
-    git push origin master
-  fi
-fi
